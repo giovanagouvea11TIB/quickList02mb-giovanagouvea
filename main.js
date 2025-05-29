bfb984ebfa515d6c1d5101a7768e38fcafb217a5
Giovana Gouvea <giovanagouvea366@gmail.com>
2025-05-29 18:41:33 -0300

    adição da função sort para mandar os itens marcados para o fim da lista

--- main.js
+++ main.js
@@ -24,6 +24,8 @@ function showItemsList() {
     const sectionList = document.querySelector(".List")
     sectionList.textContent = ""
 
+    items.sort((itemA, itemB) => Number(itemA.checked)- Number(itemB.checked))
+
     items.map((item, index) => {
         sectionList.innerHTML += `
             <div class="item">
