07a74d6240bc55c8455a0f99770f0a19f87544ca
Giovana Gouvea <giovanagouvea366@gmail.com>
2025-05-29 18:31:49 -0300

    criação da função checkItem() para mudar a propriedade checked para true ou false no array

--- main.js
+++ main.js
@@ -24,10 +24,10 @@ function showItemsList() {
             <div class="item">
                     <div>
                         <input type="checkbox" name="List" id="item-${index}">
-                        <div class="custom-checkbox">
+                        <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                             <img src="./assets/checked.svg" alt="checked">
                         </div>
-                        <label for="item-${index}">${item.name}</label>
+                        <label for="item-${index}" onclick="checkItem('${item.name}')" >${item.name}</label>
                     </div>
 
                     <button onclick ="removeItem('${item.name}')">
@@ -52,5 +52,20 @@ function removeItem(itemName) {
         items.splice(itemIndex, 1)
     }
 
+    showItemsList()
+}
+
+function checkItem(itemName){
+    const item = items.find((item) => item.name == itemName)
+
+    //if(item.checked){
+    //    item.checked = false
+    //} else {
+    //    item.checked = true
+    // }
+
+    //item.checked ? item checked = false : item.checked = true 
+
+    item.checked = !item.checked
     showItemsList()
 }
